931b05f3b937d274a00d96a3f0e78adc7f4b9af5
Conor T. Ryan <ctryan3@gmail.com>
2013-07-12 12:27:06 -0700

    Added JSONP detection

--- jsonfmt.go
+++ jsonfmt.go
@@ -13,30 +13,49 @@ import (
 
 func main() {
 
+    // parse args
     if (len(os.Args) < 2) {
         fmt.Println("Usage: jsonfmt <json-file>");
         return
     }
-
     filename := os.Args[1]
 
+    // read file
     in, err := ioutil.ReadFile(filename)
     if (err != nil) {
         log.Fatal(err)
         return
     }
 
-    out := bytes.NewBufferString("")
+    isJSONP := parseJSONP(in)
 
-    if err := json.Indent(out, in, "", "\t"); err == nil {
+    if (isJSONP == nil) {
+        printJSON(in)
+    } else {
+        fmt.Print(string(isJSONP[1]))
+        printJSON(isJSONP[2])
+        fmt.Print(string(isJSONP[3]))
+    }
+    fmt.Print("\n")
+
+}
+
+func printJSON(jsonstr []byte) {
+    out := bytes.NewBufferString("")
+    if err := json.Indent(out, jsonstr, "", "\t"); err == nil {
         io.Copy(os.Stdout, out)
     } else {
         log.Fatal(err)
     }
 }
 
-func isJSONP(b []byte) bool {
-    re := "([a-zA-Z_0-9\\.]*\\()|(\\);?$)"
-    isMatch, _ := regexp.Match(re, b);
-    return isMatch
+func parseJSONP(str []byte) [][]byte {
+    re, _ := regexp.Compile(`^([A-Za-z_0-9\.]+\()(.*)(\))$`)
+    matches := re.FindAllSubmatch(str, -1)
+    if (matches == nil) {
+        return nil
+    } else if (len(matches[0]) != 4) {
+        log.Fatal("Bad JSON!")
+    }
+    return matches[0]
 }
