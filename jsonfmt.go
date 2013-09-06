116e7de6baabdb542796a19bdeb8792d870e1f82
Conor T. Ryan <ctryan3@gmail.com>
2013-09-06 10:50:18 -0700

    JSON files now indented in-place

--- jsonfmt.go
+++ jsonfmt.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "io/ioutil"
     "os"
-    "log"
-    "regexp"
+    "io"
     "fmt"
+    "log"
+    "bytes"
+    "regexp"
+    "encoding/json"
 )
 
 func main() {
+
+    var (
+        buf bytes.Buffer
+        jsonBody []byte
+        isJSONP bool
+        jsonpParts [][]byte
+    )
 
     // parse args
     if (len(os.Args) < 2) {
@@ -19,37 +26,67 @@ func main() {
     }
     filename := os.Args[1]
 
-    // read file
-    contents, err := ioutil.ReadFile(filename)
-    if (err != nil) {
-        log.Fatal(err)
-        return
-    }
-
-    // check if it's jsonp and handle accordingly
-    if check, parts := isJSONP(contents); check == true {
-        // handle jsonp
-        fmt.Print(string(parts[1]))
-        printFormattedJSON(parts[2])
-        fmt.Print(string(parts[3]) + "\n")
-    } else {
-        // handle regular json
-        printFormattedJSON(contents)
-    }
-}
-
-func printFormattedJSON(contents []byte) {
-    formatted := bytes.NewBufferString("")
-    if err := json.Indent(formatted, contents, "", "    "); err == nil {
-        fmt.Print(formatted.String())
-    } else {
+    // open file and read it into buffer
+    fi, err := os.Open(filename)
+    if err != nil {
         log.Fatal(err)
     }
+    data := make([]byte, 1024)
+    for {
+        n, err := fi.Read(data)
+        if err != nil && err != io.EOF {
+            log.Fatal(err)
+        }
+        if n == 0 {
+            break
+        }
+        buf.Write(data[:n])
+    }
+    fi.Close()
+
+    // check if it's JSONP
+    if isJSONP, jsonpParts := checkJSONP(buf.Bytes()); isJSONP {
+        jsonBody = jsonpParts[1]
+    } else {
+        jsonBody = buf.Bytes()
+    }
+
+    // make a new buffer of indented json
+    cleanJSON := bytes.NewBufferString("")
+    err = json.Indent(cleanJSON, jsonBody, "", "    ")
+    if err != nil {
+        if serr, ok := err.(*json.SyntaxError); ok {
+            fmt.Printf("Syntax error at byte %d: %s\n", serr.Offset, serr.Error())
+        } else {
+            fmt.Println(err)
+        }
+        os.Exit(1)
+    }
+
+    if isJSONP {
+        // add the head and tail padding
+        jsonpParts[1] = cleanJSON.Bytes()
+        cleanJSON = bytes.NewBuffer(bytes.Join(jsonpParts, []byte{}))
+    }
+
+    fmt.Println(cleanJSON.String())
+
+    /*
+    // write the buffer into the same file
+    fo, err := os.Create(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fo.Write(cleanJSON.Bytes())
+    fo.Close()
+    */
 }
 
-func isJSONP(contents []byte) (bool, [][]byte) {
+func checkJSONP(contents []byte) (bool, [][]byte) {
     re, _ := regexp.Compile(`^([A-Za-z_0-9\.]+\()(.*)(\))$`)
     matches := re.FindAllSubmatch(contents, -1)
+    fmt.Println(matches)
+    os.Exit(1)
     if (matches == nil) {
         return false, nil
     } else if (len(matches[0]) != 4) {
