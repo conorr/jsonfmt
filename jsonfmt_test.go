79ec301052675ab11dc4005b74da9b18a5d8a5aa
Conor T. Ryan <ctryan3@gmail.com>
2014-01-18 12:15:22 -0800

    Add more tests

--- jsonfmt_test.go
+++ jsonfmt_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 import "bytes"
 
-func TestParseJSONP(t *testing.T) {
+func TestParseJSONP1(t *testing.T) {
 
     var buf bytes.Buffer
     var err error
@@ -11,15 +11,37 @@ func TestParseJSONP(t *testing.T) {
     buf.Write([]byte("CALLBACK({\"foo\":\"bar\"})"))
     _, err = ParseJSONP(buf.Bytes())
     if err != nil {
-        t.Fatal("Valid JSONP throwing error")
+        t.Error("Valid JSONP throwing error")
     }
+}
 
-    buf.Reset()
+func TestParseJSONP2(t *testing.T) {
+
+    var buf bytes.Buffer
+    var err error
 
     buf.Write([]byte("CALLBACK{\"foo\":\"bar\"})"))
     _, err = ParseJSONP(buf.Bytes())
     if err == nil {
-        t.Fatal("Invalid JSONP not throwing error")
+        t.Error("Invalid JSONP not throwing error")
     }
+}
 
+func TestParseJSONP3(t *testing.T) {
+
+    tests := make(map[string][]string)
+
+    // Tests
+    tests["CALLBACK({})"] = []string{"CALLBACK(", "{}", ")"}
+    tests["\nCALLBACK({})\n"] = []string{"\nCALLBACK(", "{}", ")\n"}
+    tests["\nCall.back({})\n"] = []string{"\nCall.back(", "{}", ")\n"}
+
+    for test, expect := range tests {
+        result, _ := ParseJSONP([]byte(test))
+        for i := 0; i < 3; i++ {
+            if string(result[i]) != expect[i] {
+                t.Errorf("Expected %s, got %s", expect[i], string(result[i]))
+            }
+        }
+    }
 }
