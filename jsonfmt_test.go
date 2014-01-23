79a2dcc46204261a1f6db8d0f5c2a26798dcf7cf
Conor T. Ryan <ctryan3@gmail.com>
2014-01-23 15:23:09 -0800

    Add working Indent method

--- jsonfmt_test.go
+++ jsonfmt_test.go
@@ -77,3 +77,19 @@ func TestRawInterfaceMapErrors(t *testing.T) {
     }
 
 }
+
+func TestIndent(t *testing.T) {
+
+    var buf bytes.Buffer
+    i := make(map[string]interface{})
+    i["foo"] = "bar"
+    i["num"] = 2
+    i["floaty"] = 3.14
+
+    b := make(map[string]interface{})
+    b["foo"] = "bar"
+    i["maptest"] = b
+
+    Indent(&buf, i, 0, "    ")
+    
+}
