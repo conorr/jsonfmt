7f579f234ed13c0876df50a75f8cd20a47481bae
Conor T. Ryan <ctryan3@gmail.com>
2014-01-31 16:41:38 -0800

    Add end-to-end tests for indent

--- indent_test.go
+++ indent_test.go
@@ -3,11 +3,15 @@ package main
 import (
     "bytes"
     "testing"
+    "os"
+    "log"
+    "io"
+    "fmt"
 )
 
 func TestIndent(t *testing.T) {
 
-    var buf bytes.Buffer
+    //var buf bytes.Buffer
     i := make(map[string]interface{})
     i["foo"] = "bar"
     i["num"] = 2
@@ -31,5 +35,71 @@ func TestIndent(t *testing.T) {
     arr[2] = bc
     i["arr"] = arr
 
-    Indent(&buf, i, "    ")
+    //Indent(&buf, i, "    ")
+}
+
+func TestIndentEndtoEnd(t *testing.T) {
+
+    var bufIn bytes.Buffer
+    var bufOut bytes.Buffer
+
+    // Open file and read into buffer.
+    fi, err := os.Open("testfiles/test1.json")
+    if err != nil {
+        log.Fatal(err)
+    }
+    readBytes := make([]byte, 1024)
+    for {
+        n, err := fi.Read(readBytes)
+        if err != nil && err != io.EOF {
+            log.Fatal(err)
+        }
+        if n == 0 {
+            break
+        }
+        bufIn.Write(readBytes[:n])
+    }
+    fi.Close()
+
+    obj, err := RawInterfaceMap(bufIn.Bytes())
+    if err != nil {
+        t.Errorf("RawInterfaceMap returned error; possible syntax error")
+        return
+    }
+    Indent(&bufOut, obj, "    ")
+
+    // Trim newlines off right of file
+    for index, expect := range bytes.TrimRight(bufIn.Bytes(), "\n") {
+        if index > len(bufOut.Bytes()) + 1 {
+            t.Errorf("bufOut was smaller than bufIn!")
+            break
+        }
+        result := bufOut.Bytes()[index]
+        if expect != result {
+            t.Errorf("Expecting '%s', got '%s' at byte %d", string(expect),
+                string(result), index)
+            fmt.Println(string(bufOut.Bytes()))
+            break
+        }
+    }
+}
+
+func TestWritef(t *testing.T) {
+
+    var buf bytes.Buffer
+
+    Writef(&buf, "hello")
+    str := string(buf.Bytes())
+    if str != "hello" {
+        t.Errorf("Error!")
+    }
+
+    buf.Reset()
+    
+    Writef(&buf, "\"%s\"", "hello")
+    str = string(buf.Bytes())
+    if str != "\"hello\"" {
+        t.Errorf("Error!")
+    }
+
 }
