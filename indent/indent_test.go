56131154bc88d3867283bd1eb5b31a82f1097308
Conor T. Ryan <ctryan3@gmail.com>
2014-02-01 20:08:24 -0800

    Remove dead code

--- indent/indent_test.go
+++ indent/indent_test.go
@@ -10,35 +10,6 @@ import (
     "jsonfmt/decode"
 )
 
-func TestIndent(t *testing.T) {
-
-    //var buf bytes.Buffer
-    i := make(map[string]interface{})
-    i["foo"] = "bar"
-    i["num"] = 2
-    i["floaty"] = 3.14
-    i["bool"] = false
-
-    b := make(map[string]interface{})
-    b["foo"] = "bar"
-    b["ima"] = "map"
-    b["ican"] = 24
-    i["maptest"] = b
-
-    bc := make(map[string]interface{})
-    bc["apples"] = "oranges"
-    bc["bananas"] = "pineapples"
-    i["fruits"] = bc
-
-    arr := make([]interface{}, 3)
-    arr[0] = bc
-    arr[1] = bc
-    arr[2] = bc
-    i["arr"] = arr
-
-    //Indent(&buf, i, "    ")
-}
-
 func TestIndentEndtoEnd(t *testing.T) {
 
     var bufIn bytes.Buffer
@@ -84,25 +55,3 @@ func TestIndentEndtoEnd(t *testing.T) {
         }
     }
 }
-
-/*
-func TestWritef(t *testing.T) {
-
-    var buf bytes.Buffer
-
-    Writef(&buf, "hello")
-    str := string(buf.Bytes())
-    if str != "hello" {
-        t.Errorf("Error!")
-    }
-
-    buf.Reset()
-    
-    Writef(&buf, "\"%s\"", "hello")
-    str = string(buf.Bytes())
-    if str != "\"hello\"" {
-        t.Errorf("Error!")
-    }
-
-}
-*/
